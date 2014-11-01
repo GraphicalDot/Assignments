9865dbfb17705442fd82a6c90002d646a0326b03
kaali-python <root@kaali.(none)>
2014-11-01 17:51:47 +0530

    first assignments

---
+++ assignment_1_nov_2014.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+def function_one(number):
+	"""
+	Print 10 times this statement with variables names like
+	i am number 1
+	i am number 2
+	
+	.
+	.
+	.
+	.
+	i am number number
+
+	"""
+	pass
+
+def function_time(time):
+	"""
+	When given args = 10 july 2014
+	convert it into epoch time
+	"""
+	pass
+
+
+
+def exception_handling():
+	"""
+	Handle exception
+	"""
+	pass
+
+
+def open_file(file_path):
+	"""
+	Print the contents of the file ying on file_path
+	"""
+
+
+def this_file_path():
+	"""
+	Print the directory of this file 
+	"""
+	pass
+
+
+def convert_string_to_list(string):
+	"""
+	Convert this string into a list 
+	string = "HEY_MAN_WHAT_the_fuck_is_going_on"
+	output = ["Hey", "man", "what", "the", "fuck", "is"m "going", "on"]
+	
+	The convert this list into 
+	string = "hey man what the fuck is going on""
+
+	Sonvert this string into
+	string = "hey man, everything is great"
+
+	"""
+	pass
+
+
+if  __name__ == "__main__":
+
+	
+
+
